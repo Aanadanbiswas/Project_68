4eeb19c480438cee957f2e0d302589200503ae79
Aanadanbiswas <67957902+Aanadanbiswas@users.noreply.github.com>
2021-05-01 09:31:39 +0530

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+class FacebookScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Facebook</Text>
+            </View>
+        )
+    }
+}
+
+export default FacebookScreen
---
+++ screens/in.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+class InstagramScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Instagram</Text>
+            </View>
+        )
+    }
+}
+
+export default InstagramScreen
